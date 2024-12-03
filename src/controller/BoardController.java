64efcf0850c7c01886ce225842760272a5f03174
hahnaughton <hahnaughton@gmail.com>
2024-12-03 10:17:05 -0700

    Fixed grid background layout

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -5,57 +5,63 @@ import model.*;
 
 public class BoardController {
 
-    Board board;
+  Board board;
 
-    public BoardController() {
-        board = new Board();
-    }
+  public BoardController() {
+    board = new Board();
+  }
 
-    public void setBoard(Board board) {
-        this.board = board;
-    }
+  public void setBoard(Board board) {
+    this.board = board;
+  }
 
-    public boolean moveUp() {
-        return board.move(Directions.UP);
-    }
+  public boolean moveUp() {
+    return board.move(Directions.UP);
+  }
 
-    public boolean moveDown() {
-        return board.move(Directions.DOWN);
-    }
+  /*************  ✨ Codeium Command ⭐  *************/
+  /**
+   * Moves the tiles down.
+   * @return true if the board state changed (i.e. a tile was moved or merged), false otherwise
+   */
+  /******  d974140c-7252-4e2d-a650-ce56d20c31b4  *******/
+  public boolean moveDown() {
+    return board.move(Directions.DOWN);
+  }
 
-    public boolean moveLeft() {
-        return board.move(Directions.LEFT);
-    }
+  public boolean moveLeft() {
+    return board.move(Directions.LEFT);
+  }
 
-    public boolean moveRight() {
-        return board.move(Directions.RIGHT);
-    }
+  public boolean moveRight() {
+    return board.move(Directions.RIGHT);
+  }
 
-    public List<Integer> getBoard() {
-        return board.getBoardValues();
-    }
+  public List<Integer> getBoard() {
+    return board.getBoardValues();
+  }
 
-    public void addRandomTile() {
-        board.addRandomTile();
-    }
+  public void addRandomTile() {
+    board.addRandomTile();
+  }
 
-    public int getCurrentScore() {
-        return board.getCurrentScore();
-    }
+  public int getCurrentScore() {
+    return board.getCurrentScore();
+  }
 
-    public String toString() {
-        return board.toString();
-    }
+  public String toString() {
+    return board.toString();
+  }
 
-    public void setTile(int x, int y, Tile tile) {
-        board.setTile(x, y, tile);
-    }
+  public void setTile(int x, int y, Tile tile) {
+    board.setTile(x, y, tile);
+  }
 
-    public Tile getTile(int x, int y) {
-        return board.getTile(x, y);
-    }
+  public Tile getTile(int x, int y) {
+    return board.getTile(x, y);
+  }
 
-    public boolean isGameOver() {
-        return board.isGameOver();
-    }
+  public boolean isGameOver() {
+    return board.isGameOver();
+  }
 }
