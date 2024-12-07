8cedc63fd48e3ee1f647489f6515cb71bd56644c
hahnaughton <hahnaughton@gmail.com>
2024-12-06 19:43:02 -0700

    Added leaderboard

--- src/controller/FriendDatabaseController.java
+++ src/controller/FriendDatabaseController.java
@@ -2,126 +2,129 @@ package controller;
 
 import java.io.IOException;
 import java.util.List;
-
 import model.FriendDatabase;
 import model.User;
 
 public class FriendDatabaseController {
 
-    private UserController userController;
+  private UserController userController;
 
-    /**
-     * @param userController the user controller to use when adding and removing
-     *                       friends
-     * @pre userController != null
-     */
-    public FriendDatabaseController(UserController userController) {
-        this.userController = userController;
-    }
+  /**
+   * @param userController the user controller to use when adding and removing
+   *                       friends
+   * @pre userController != null
+   */
+  public FriendDatabaseController(UserController userController) {
+    this.userController = userController;
+  }
 
-    /**
-     * @param username       the username of the user who is adding the friend
-     * @param friendUsername the username of the friend being added
-     * @return true if the friend was added successfully, false if the friend could
-     *         not
-     *         be added
-     * @pre userController != null
-     * @pre username != null and not empty
-     * @pre friendUsername != null and not empty
-     */
-    public boolean addFriend(String username, String friendUsername) {
-        User friendUser;
-        User adderUser;
-        friendUser = this.userController.getUser(friendUsername);
-        adderUser = this.userController.getUser(username);
-        if (friendUser == null) {
-            return false;
-        }
-        return FriendDatabase.addFriend(username, friendUser)
-                && FriendDatabase.addFriend(friendUser.getUsername(), adderUser);
+  /**
+   * @param username       the username of the user who is adding the friend
+   * @param friendUsername the username of the friend being added
+   * @return true if the friend was added successfully, false if the friend could
+   *         not
+   *         be added
+   * @pre userController != null
+   * @pre username != null and not empty
+   * @pre friendUsername != null and not empty
+   */
+  public boolean addFriend(String username, String friendUsername) {
+    User friendUser;
+    User adderUser;
+    friendUser = this.userController.getUser(friendUsername);
+    adderUser = this.userController.getUser(username);
+    if (friendUser == null) {
+      return false;
     }
+    return (
+      FriendDatabase.addFriend(username, friendUser) &&
+      FriendDatabase.addFriend(friendUser.getUsername(), adderUser)
+    );
+  }
 
-    /**
-     * @param username       the username of the user who is removing the friend
-     * @param friendUsername the username of the friend being removed
-     * @return true if the friend was removed successfully, false if the friend
-     *         could
-     *         not be removed
-     * @pre userController != null
-     * @pre username != null and not empty
-     * @pre friendUsername != null and not empty
-     */
-    public boolean removeFriend(String username, String friendUsername) {
-        User friendUser;
-        User removeUser;
-        friendUser = this.userController.getUser(friendUsername);
-        removeUser = this.userController.getUser(username);
-        if (friendUser == null || removeUser == null) {
-            return false;
-        }
-        return FriendDatabase.removeFriend(username, friendUser)
-                && FriendDatabase.removeFriend(friendUser.getUsername(), removeUser);
+  /**
+   * @param username       the username of the user who is removing the friend
+   * @param friendUsername the username of the friend being removed
+   * @return true if the friend was removed successfully, false if the friend
+   *         could
+   *         not be removed
+   * @pre userController != null
+   * @pre username != null and not empty
+   * @pre friendUsername != null and not empty
+   */
+  public boolean removeFriend(String username, String friendUsername) {
+    User friendUser;
+    User removeUser;
+    friendUser = this.userController.getUser(friendUsername);
+    removeUser = this.userController.getUser(username);
+    if (friendUser == null || removeUser == null) {
+      return false;
     }
+    return (
+      FriendDatabase.removeFriend(username, friendUser) &&
+      FriendDatabase.removeFriend(friendUser.getUsername(), removeUser)
+    );
+  }
 
-    /**
-     * @param username the username of the user whose friends are to be retrieved
-     * @return the list of friends for the given user
-     * @pre username is non-null and not empty
-     * @post the list of friends is not null and is not empty if the user has
-     *       friends
-     */
-    public List<User> getFriends(String username) {
-        return FriendDatabase.getFriends(username);
-    }
+  /**
+   * @param username the username of the user whose friends are to be retrieved
+   * @return the list of friends for the given user
+   * @pre username is non-null and not empty
+   * @post the list of friends is not null and is not empty if the user has
+   *       friends
+   */
+  public List<User> getFriends(String username) {
+    return FriendDatabase.getFriends(username);
+  }
 
-    /**
-     * Saves the friends database to the specified file.
-     * 
-     * @param friendFile the file to save the friends database to
-     * @pre friendFile is non-null and not empty
-     * @post the friends database has been saved to the specified file
-     */
-    public void saveFriends(String friendFile) {
-        try {
-            FriendDatabase.saveFriends(friendFile);
-        } catch (IOException e) {
-            System.err.println("Error saving friends: " + e.getMessage());
-        }
+  /**
+   * Saves the friends database to the specified file.
+   *
+   * @param friendFile the file to save the friends database to
+   * @pre friendFile is non-null and not empty
+   * @post the friends database has been saved to the specified file
+   */
+  public void saveFriends(String friendFile) {
+    try {
+      FriendDatabase.saveFriends(friendFile);
+    } catch (IOException e) {
+      System.err.println("Error saving friends: " + e.getMessage());
     }
+  }
 
-    /**
-     * Loads the friends database from the specified files.
-     * 
-     * @param userFile   the file containing user data
-     * @param friendFile the file containing friend relationships
-     * @pre userFile and friendFile are non-null and not empty
-     * @post the friends database has been loaded from the specified files
-     */
-    public void loadFriends(String userFile, String friendFile) {
-        try {
-            FriendDatabase.loadFriends(userFile, friendFile);
-        } catch (IOException e) {
-            System.err.println("Error loading friends: " + e.getMessage());
-        }
+  /**
+   * Loads the friends database from the specified files.
+   *
+   * @param userFile   the file containing user data
+   * @param friendFile the file containing friend relationships
+   * @pre userFile and friendFile are non-null and not empty
+   * @post the friends database has been loaded from the specified files
+   */
+  public void loadFriends(String userFile, String friendFile) {
+    try {
+      FriendDatabase.loadFriends(userFile, friendFile);
+    } catch (IOException e) {
+      System.err.println("Error loading friends: " + e.getMessage());
     }
+  }
 
-    /**
-     * Prints the friends database.
-     * 
-     * @pre The friends database is initialized
-     * @post The friends database has been printed to the console
-     */
-    public void print() {
-        FriendDatabase.print();
-    }
+  /**
+   * Prints the friends database.
+   *
+   * @pre The friends database is initialized
+   * @post The friends database has been printed to the console
+   */
+  public void print() {
+    FriendDatabase.print();
+  }
 
-    /**
-     * Clears the friends database.
-     * 
-     * @pre The friends database is initialized
-     * @post The friends database is empty
-     */
-    public void clear() {
-        FriendDatabase.clear();
-    }
+  /**
+   * Clears the friends database.
+   *
+   * @pre The friends database is initialized
+   * @post The friends database is empty
+   */
+  public void clear() {
+    FriendDatabase.clear();
+  }
 }
--- src/view/TextUI.java
+++ src/view/TextUI.java
@@ -1,108 +1,131 @@
 package view;
 
-import java.io.File;
-import java.util.Scanner;
 import controller.BoardController;
 import controller.UserController;
+import java.io.File;
+import java.util.Scanner;
 import model.User;
 
 public class TextUI {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        File usersFile = new File("src/files/UserDatabase.csv");
-        BoardController boardController = new BoardController();
-        UserController userController = new UserController(usersFile);
-        System.out.println("Welcome to 2048!");
-        User currentUser = null;
 
-        // Start the game loop
-        boardController.addRandomTile();
-        boardController.addRandomTile();
-        boolean playing = true;
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    File usersFile = new File("src/files/UserDatabase.csv");
+    BoardController boardController = new BoardController();
+    UserController userController = new UserController(usersFile);
+    System.out.println("Welcome to 2048!");
+    User currentUser = null;
 
-        while (playing) {
-            System.out.println("Current Board:");
-            System.out.println(boardController);
+    // Start the game loop
+    boardController.addRandomTile();
+    boardController.addRandomTile();
+    boolean playing = true;
 
-            System.out.println("Enter a move (w: up, s: down, a: left, d: right, q: quit): ");
-            String move = scanner.nextLine();
+    while (playing) {
+      System.out.println("Current Board:");
+      System.out.println(boardController);
 
-            switch (move.toLowerCase()) {
-                case "w":
-                    boardController.moveUp();
-                    break;
-                case "s":
-                    boardController.moveDown();
-                    break;
-                case "a":
-                    boardController.moveLeft();
-                    break;
-                case "d":
-                    boardController.moveRight();
-                    break;
-                case "q":
-                    playing = false;
-                    break;
-                default:
-                    System.out.println("Invalid move. Please try again.");
-            }
+      System.out.println(
+        "Enter a move (w: up, s: down, a: left, d: right, q: quit): "
+      );
+      String move = scanner.nextLine();
 
-            if (boardController.isGameOver()) {
-                System.out.println("Game over! Your final score is: " + boardController.getCurrentScore());
-                playing = false;
-            }
-        }
+      switch (move.toLowerCase()) {
+        case "w":
+          boardController.moveUp();
+          break;
+        case "s":
+          boardController.moveDown();
+          break;
+        case "a":
+          boardController.moveLeft();
+          break;
+        case "d":
+          boardController.moveRight();
+          break;
+        case "q":
+          playing = false;
+          break;
+        default:
+          System.out.println("Invalid move. Please try again.");
+      }
 
-        int score = boardController.getCurrentScore();
-        System.out.println("Do you have an account? (y/n)");
-        String response = scanner.nextLine();
-        if (response.equalsIgnoreCase("y")) {
-            currentUser = signIn(scanner, userController, score);
-        } else if (response.equalsIgnoreCase("n")) {
-            currentUser = signUp(scanner, userController, score);
-        } else {
-            System.out.println("Invalid response. Please try again.");
-        }
-
-        System.out.println("Your current score is: " + boardController.getCurrentScore());
-        if (currentUser != null) {
-            System.out.println("Your highest score is: " + currentUser.getHighestScore());
-        }
-
-        System.out.println("Thanks for playing 2048!");
-        scanner.close();
+      if (boardController.isGameOver()) {
+        System.out.println(
+          "Game over! Your final score is: " + boardController.getCurrentScore()
+        );
+        playing = false;
+      }
     }
 
-    private static User signIn(Scanner scanner, UserController userController, int score) {
-        System.out.println("Enter your username: ");
-        String username = scanner.nextLine();
-        System.out.println("Enter your password: ");
-        String password = scanner.nextLine();
-        User currentUser = userController.getUser(username, password);
-        if (currentUser == null) {
-            System.out.println("Invalid username or password. Please try again.");
-        } else {
-            currentUser.setHighestScore(score);
-            userController.updateUser(username, password, score);
-        }
-        return currentUser;
+    int score = boardController.getCurrentScore();
+    System.out.println("Do you have an account? (y/n)");
+    String response = scanner.nextLine();
+    if (response.equalsIgnoreCase("y")) {
+      currentUser = signIn(scanner, userController, score);
+    } else if (response.equalsIgnoreCase("n")) {
+      currentUser = signUp(scanner, userController, score);
+    } else {
+      System.out.println("Invalid response. Please try again.");
     }
 
-    private static User signUp(Scanner scanner, UserController userController, int score) {
-        System.out.println("Enter your username: ");
-        String username = scanner.nextLine();
-        System.out.println("Enter your password: ");
-        String password = scanner.nextLine();
-        System.out.println("Enter your email: ");
-        String email = scanner.nextLine();
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        boolean success = userController.addUser(username, password, email, name, score);
-        if (!success) {
-            System.out.println("Username already exists. Please try again.");
-        } else {
-            System.out.println("Account created successfully!");
-        }
-        return userController.getUser(username, password);
+    System.out.println(
+      "Your current score is: " + boardController.getCurrentScore()
+    );
+    if (currentUser != null) {
+      System.out.println(
+        "Your highest score is: " + currentUser.getHighestScore()
+      );
     }
+
+    System.out.println("Thanks for playing 2048!");
+    scanner.close();
+  }
+
+  private static User signIn(
+    Scanner scanner,
+    UserController userController,
+    int score
+  ) {
+    System.out.println("Enter your username: ");
+    String username = scanner.nextLine();
+    System.out.println("Enter your password: ");
+    String password = scanner.nextLine();
+    User currentUser = userController.getUser(username, password);
+    if (currentUser == null) {
+      System.out.println("Invalid username or password. Please try again.");
+    } else {
+      currentUser.setHighestScore(score);
+      userController.updateUser(username, password, score);
+    }
+    return currentUser;
+  }
+
+  private static User signUp(
+    Scanner scanner,
+    UserController userController,
+    int score
+  ) {
+    System.out.println("Enter your username: ");
+    String username = scanner.nextLine();
+    System.out.println("Enter your password: ");
+    String password = scanner.nextLine();
+    System.out.println("Enter your email: ");
+    String email = scanner.nextLine();
+    System.out.println("Enter your name: ");
+    String name = scanner.nextLine();
+    boolean success = userController.addUser(
+      username,
+      password,
+      email,
+      name,
+      score
+    );
+    if (!success) {
+      System.out.println("Username already exists. Please try again.");
+    } else {
+      System.out.println("Account created successfully!");
+    }
+    return userController.getUser(username, password);
+  }
 }
